7b2864bed1fb7cd6039fb4c02e9764c9f96b2df6
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2022-04-29 20:11:54 +0530

    Folder Structure and Assets

---
+++ src/assests/indexImg.js
@@ -0,0 +1,7 @@
+import profilePic from "./profilePic.jpg";
+import abstract from "./abstract.svg";
+import polygonSvg from "./polygon.svg";
+import metaMaskSvg from "./metaMask.svg";
+import randomSvg from "./random.svg";
+
+export { profilePic, abstract, polygonSvg, metaMaskSvg,randomSvg };
